12e3fad72b8a09640c099e4b51fed88219cc21e3
George Cohen <georgercohen@gmail.com>
2026-01-21 20:39:37 -0800

    Beginning functions implemented (To work on other computer)

---
+++ Public/index.js
@@ -0,0 +1,6 @@
+const protocol = window.location.protocol === 'https:' ? 'wss:' : 'ws:';
+const ws = new WebSocket(`${protocol}//${window.location.host}`);
+
+ws.onopen = () => {
+    ws.send(JSON.stringify({ event: "userJoined" }))
+}
--- app.js
+++ app.js
@@ -1,17 +1,65 @@
+const WebSocket = require("ws")
+const express = require("express")
+const http = require("http")
+
+const app = express()
+const server = http.createServer(app)
+
+const wss = new WebSocket.Server({ server });
+app.use(express.static("public"))
+const PORT = process.env.PORT || 3000
+
 const suits = ["clubs", "diamonds", "hearts", "spades"]
 const ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "A", "J", "K", "Q"]
-const deck = []
+let playerCount = 0
 
-suits.forEach(suit => {
-    ranks.forEach(rank => {
-        deck.push({
-            suit: suit,
-            rank: rank,
-            image: `Assets/${suit}_${rank}.png`
-        })
+
+wss.on("connection", (ws) => {
+    console.log("Up and running")
+    ws.on("message", (data) => {
+        const msg = JSON.parse(data.toString())
+        if (msg.event == "userJoined") {
+            playerCount += 1
+            console.log(playerCount)
+        }
+    })
+
+
+    ws.on("close", () => {
+        playerCount -= 1
+        console.log(playerCount)
     })
 })
 
+
+
+    // const deck = createDeck()
+    // const shuffledDeck = shuffle(deck)
+    // console.log(shuffledDeck)
+
+
+
+
+
+
+
+// Initialize Deck
+function createDeck() {
+    const deck = []
+    suits.forEach(suit => {
+        ranks.forEach(rank => {
+            deck.push({
+                suit: suit,
+                rank: rank,
+                image: `Assets/${suit}_${rank}.png`
+            })
+        })
+    })
+
+    return deck
+}
+
+// Randomly Shuffle Deck
 function shuffle(deck) {
     let shuffledDeck = []
     for (i = deck.length; i > 0; i--) {
@@ -24,5 +72,8 @@ function shuffle(deck) {
 }
 
 
-let shuffledDeck = shuffle(deck)
-console.log(shuffledDeck)
+// <img src="Assets/playing-cards-master/back_dark.png" class="card" alt="Description of the image">
+
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
